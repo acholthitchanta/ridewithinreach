652119bce991d62f640d1a774feb5218fefe5dd1
acholthitchanta <78499478+acholthitchanta@users.noreply.github.com>
2026-08-23 17:22:01 -0500

    f

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <section className="section green">
-        <h1>HELLO</h1>
-    </section>
+    <footer className="green section">
+        <div className="contact text">
+            <h2>Ride Within Reach</h2>
+            <div className="links">
+                <p><a>MEET THE TEAM</a></p>
+                <p><a>OUR WORK</a></p>
+                <p><a>OUR SUPPORTERS</a></p>
+                <p><a>SPONSOR A RIDER</a></p>
+            </div>
+        </div>
+
+        <div className="text">
+            <h2>EXPLORE</h2>
+            <div className="links">
+                <p><a>MEET THE TEAM</a></p>
+                <p><a>OUR WORK</a></p>
+                <p><a>OUR SUPPORTERS</a></p>
+                <p><a>SPONSOR A RIDER</a></p>
+            </div>
+        </div>
+    </footer>
   )
 }
